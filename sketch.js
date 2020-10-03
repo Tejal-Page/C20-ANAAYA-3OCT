8a49fb722ba59ab98cde96942e29588e35b25ddc
anaaya2501 <71642248+anaaya2501@users.noreply.github.com>
2020-10-03 11:45:40 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,24 @@
+var rect1,rect2;
+function setup() {
+  createCanvas(800,400);
+  rect1 =  createSprite(400, 200, 50, 70);
+  rect2 = createSprite(400,200,50,70);
+
+}
+
+function draw() {
+  background(255,255,255);  
+  rect1.y = World.mouseY;
+  rect1.x = World.mouseX;
+
+if(rect1.x-rect2.x<rect2.width/2 && rect2.x-rect1.x<rect1.width/2 && rect2.y-rect2.y<rect2.height/2 && rect2.y-rect1.y<rect1.height/2){
+rect1.shapeColor = "red";
+rect2.shapeColor = "blue";
+
+}
+else {
+  rect1.shapeColor = "grey";
+  rect2.shapeColor = "grey";
+}
+  drawSprites();
+}
